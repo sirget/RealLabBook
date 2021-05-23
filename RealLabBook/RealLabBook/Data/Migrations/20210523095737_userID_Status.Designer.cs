9c0dc101bc665c91d7e3cba11b85c83f3a3215a8
bestvarut <51030057+bestvarut@users.noreply.github.com>
2021-05-23 17:02:41 +0700

    basic authen

--- RealLabBook/RealLabBook/Data/Migrations/20210523095737_userID_Status.Designer.cs
+++ RealLabBook/RealLabBook/Data/Migrations/20210523095737_userID_Status.Designer.cs
@@ -3,15 +3,17 @@ using System;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 using RealLabBook.Data;
 
 namespace RealLabBook.Data.Migrations
 {
     [DbContext(typeof(ApplicationDbContext))]
-    partial class ApplicationDbContextModelSnapshot : ModelSnapshot
+    [Migration("20210523095737_userID_Status")]
+    partial class userID_Status
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
