df34ddb97e8d55f0b91fb194830cd99c13ea2871
bestvarut <51030057+bestvarut@users.noreply.github.com>
2021-05-23 16:23:14 +0700

    Update program for seed

--- RealLabBook/RealLabBook/Program.cs
+++ RealLabBook/RealLabBook/Program.cs
@@ -6,6 +6,8 @@ using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using RealLabBook.Data;
+using RealLabBook.Models;
 
 namespace RealLabBook
 {
@@ -13,7 +15,24 @@ namespace RealLabBook
     {
         public static void Main(string[] args)
         {
-            CreateHostBuilder(args).Build().Run();
+            var host = CreateHostBuilder(args).Build();
+
+            using (var scope = host.Services.CreateScope())
+            {
+                var services = scope.ServiceProvider;
+
+                try
+                {
+                    Seed.Initialize(services);
+                }
+                catch (Exception ex)
+                {
+                    var logger = services.GetRequiredService<ILogger<Program>>();
+                    logger.LogError(ex, "An error occurred seeding the DB.");
+                }
+            }
+
+            host.Run();
         }
 
         public static IHostBuilder CreateHostBuilder(string[] args) =>
