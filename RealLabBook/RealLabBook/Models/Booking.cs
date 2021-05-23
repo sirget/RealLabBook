25b6bd7f4dfb5bdef938f9c87a7e70f02e0b7053
sirget <37872290+sirget@users.noreply.github.com>
2021-05-23 18:22:15 +0700

    upload

---
+++ RealLabBook/RealLabBook/Models/Booking.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using System.ComponentModel.DataAnnotations;
+
+namespace RealLabBook.Models
+{
+    public class Booking
+    {
+        [Key]
+        public int BookingID { get; set; }
+        public int ToolID { get; set; }
+        public string UserID { get; set; }
+        public string start_time { get; set; }
+    }
+}
