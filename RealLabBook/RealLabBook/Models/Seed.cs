ca357d2c3b3ab3d87dc0fc7fccd5755772ad0c99
bestvarut <51030057+bestvarut@users.noreply.github.com>
2021-05-23 16:25:58 +0700

    seed

--- RealLabBook/RealLabBook/Models/Seed.cs
+++ RealLabBook/RealLabBook/Models/Seed.cs
@@ -1,11 +1,68 @@
-﻿using System;
-using System.Collections.Generic;
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.DependencyInjection;
+using RealLabBook.Data;
+using System;
 using System.Linq;
-using System.Threading.Tasks;
 
 namespace RealLabBook.Models
 {
-    public class Seed
+    public static class Seed
     {
+        public static void Initialize(IServiceProvider serviceProvider)
+        {
+            using (var context = new ApplicationDbContext(
+                serviceProvider.GetRequiredService<
+                    DbContextOptions<ApplicationDbContext>>()))
+            {
+                // Look for any movies.
+                if (context.Tool.Any())
+                {
+                    return;   // DB has been seeded
+                }
+                else
+                {
+                context.Tool.AddRange(
+                                        new Tool
+                                        {
+                                            ToolID = 1,
+                                            Toolname = "Voltmeter",
+                                            Quantity = 20
+                                        },
+
+                                        new Tool
+                                        {
+                                            ToolID = 2,
+                                            Toolname = "Arduino",
+                                            Quantity = 20
+                                        },
+
+                                        new Tool
+                                        {
+                                            ToolID = 3,
+                                            Toolname = "Oscilloscope",
+                                            Quantity = 20
+                                        },
+
+                                        new Tool
+                                        {
+                                            ToolID = 4,
+                                            Toolname = "Soldering Iron",
+                                            Quantity = 20
+                                        },
+
+                                        new Tool
+                                        {
+                                            ToolID = 5,
+                                            Toolname = "Pliers",
+                                            Quantity = 20
+                                        }
+                                    );
+                    context.SaveChanges();
+                }
+
+                
+                
+            }
+        }
     }
 }
