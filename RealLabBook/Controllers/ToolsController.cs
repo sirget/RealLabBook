c3004e87a709e5e26731f5d5d91a94b3841fcd75
bestvarut <51030057+bestvarut@users.noreply.github.com>
2021-05-25 03:55:51 +0700

    fix view and controll

---
+++ RealLabBook/Controllers/ToolsController.cs
@@ -0,0 +1,163 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Rendering;
+using Microsoft.EntityFrameworkCore;
+using RealLabBook.Data;
+using RealLabBook.Models;
+using Microsoft.AspNetCore.Authorization;
+
+namespace RealLabBook.Controllers
+{
+    public class ToolsController : Controller
+    {
+        private readonly ApplicationDbContext _context;
+
+        public ToolsController(ApplicationDbContext context)
+        {
+            _context = context;
+        }
+
+        // GET: Tools
+        public async Task<IActionResult> Index()
+        {
+            List<Tool> tools = await _context.Tools.ToListAsync();
+            int[] quan = { tools[0].Quantity, tools[1].Quantity, tools[2].Quantity, tools[3].Quantity, tools[4].Quantity, };
+            List<int> ListQuan = new List<int>();
+            for (int i = 0; i < 5; i++)
+            {
+                ListQuan.Add(quan[i]);
+            }
+            ViewData["ListQuan"] = ListQuan;
+            return View(await _context.Tools.ToListAsync());
+        }
+
+        // GET: Tools/Details/5
+        public async Task<IActionResult> Details(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var tool = await _context.Tools
+                .FirstOrDefaultAsync(m => m.ToolID == id);
+            if (tool == null)
+            {
+                return NotFound();
+            }
+
+            return View(tool);
+        }
+
+        [Authorize(Roles = "Admin")]
+        // GET: Tools/Create
+        public IActionResult Create()
+        {
+            return View();
+        }
+        [Authorize(Roles = "Admin")]
+        // POST: Tools/Create
+        // To protect from overposting attacks, enable the specific properties you want to bind to.
+        // For more details, see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Create([Bind("ToolID,Toolname,Quantity")] Tool tool)
+        {
+            if (ModelState.IsValid)
+            {
+                _context.Add(tool);
+                await _context.SaveChangesAsync();
+                return RedirectToAction(nameof(Index));
+            }
+            return View(tool);
+        }
+        [Authorize(Roles = "Admin")]
+        // GET: Tools/Edit/5
+        public async Task<IActionResult> Edit(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var tool = await _context.Tools.FindAsync(id);
+            if (tool == null)
+            {
+                return NotFound();
+            }
+            return View(tool);
+        }
+        [Authorize(Roles = "Admin")]
+        // POST: Tools/Edit/5
+        // To protect from overposting attacks, enable the specific properties you want to bind to.
+        // For more details, see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Edit(int id, [Bind("ToolID,Toolname,Quantity")] Tool tool)
+        {
+            if (id != tool.ToolID)
+            {
+                return NotFound();
+            }
+
+            if (ModelState.IsValid)
+            {
+                try
+                {
+                    _context.Update(tool);
+                    await _context.SaveChangesAsync();
+                }
+                catch (DbUpdateConcurrencyException)
+                {
+                    if (!ToolExists(tool.ToolID))
+                    {
+                        return NotFound();
+                    }
+                    else
+                    {
+                        throw;
+                    }
+                }
+                return RedirectToAction(nameof(Index));
+            }
+            return View(tool);
+        }
+        [Authorize(Roles = "Admin")]
+        // GET: Tools/Delete/5
+        public async Task<IActionResult> Delete(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var tool = await _context.Tools
+                .FirstOrDefaultAsync(m => m.ToolID == id);
+            if (tool == null)
+            {
+                return NotFound();
+            }
+
+            return View(tool);
+        }
+        [Authorize(Roles = "Admin")]
+        // POST: Tools/Delete/5
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> DeleteConfirmed(int id)
+        {
+            var tool = await _context.Tools.FindAsync(id);
+            _context.Tools.Remove(tool);
+            await _context.SaveChangesAsync();
+            return RedirectToAction(nameof(Index));
+        }
+
+        private bool ToolExists(int id)
+        {
+            return _context.Tools.Any(e => e.ToolID == id);
+        }
+    }
+}
---
+++ RealLabBook/wwwroot/js/time-card.js
@@ -0,0 +1,332 @@
+﻿const template = document.createElement("template");
+template.innerHTML = `
+<style>@import url("https://fonts.googleapis.com/css2?family=Roboto&display=swap");
+
+.hour-card {
+  font-family: "Roboto", sans-serif;
+  width: 100px;
+  height: 126px;
+  background-color: #009688;
+  margin: 5px;
+  border-radius: 20px;
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+}
+
+.hour-card .time {
+  border: 1px solid #009688;
+  box-sizing: border-box;
+  border-radius: 20px;
+  width: 100px;
+  height: 100px;
+  background-color: #ffffff;
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  justify-content: center;
+}
+.hour-card{
+    cursor:pointer;
+}
+.hour-card .start,
+.end {
+  font-size: medium;
+  margin: 0;
+  font-weight: bold;
+}
+
+.hour-card .start {
+  color: #009688;
+  font-size: 24px;
+}
+
+.hour-card .end {
+  color: #263238;
+}
+
+.hour-card .remain {
+  color: white;
+  font-size: 10px;
+}
+
+/* selected styles */
+.selected {
+  background-color: #009688;
+}
+
+.selected .time {
+  background-color: #00bfa5;
+  border: none;
+}
+
+.selected .start,
+.selected .end,
+.selected .remain {
+  color: white;
+}
+
+/* disabled styles */
+.disabled {
+  background: #d4d6d7;
+  cursor:default;
+}
+
+.disabled .time {
+  background-color: white;
+  border: 1px solid #d4d6d7;
+  box-sizing: border-box;
+}
+
+.disabled .start,
+.disabled .end{
+  color: #d4d6d7;
+}
+
+.disabled .remain{
+    color: white;
+}
+
+</style>
+<div class="hour-card " id="hour-card">
+        <div class="time">
+            <p class="hour start" id="start_hour">8:00</p>
+            <p class="hour end" id="end_hour">-9:00</p>
+        </div>
+        <p class="remain">remain: <span class="vol" id="vol">10</span></p>
+    </div>`;
+class TimeCard extends HTMLElement {
+    constructor() {
+        super();
+        var enable = true;
+        var click = false;
+        this.attachShadow({ mode: "open" });
+        this.shadowRoot.appendChild(template.content.cloneNode(true));
+        this.shadowRoot.getElementById("start_hour").innerText =
+            this.getAttribute("hour");
+        this.shadowRoot.getElementById("end_hour").innerText =
+            this.getAttribute("e_hour");
+
+        if (this.getAttribute("enough") == "true") {
+            this.enable = true;
+            this.shadowRoot.getElementById("hour-card").setAttribute("class", "hour-card ");
+        }
+        else {
+            this.enable = false;
+            this.shadowRoot.getElementById("hour-card").setAttribute("class", "hour-card disabled");
+        }
+
+    }
+    handleClick() {
+
+        if (this.enable == true) {
+
+            if (this.check == true) {
+                this.setAttribute("select", "false");
+                this.shadowRoot.getElementById("hour-card").setAttribute("class", "hour-card ");
+
+            }
+            else {
+                this.setAttribute("select", "true");
+                this.shadowRoot.getElementById("hour-card").setAttribute("class", "hour-card selected");
+
+            }
+            this.check = !this.check;
+
+        }
+    }
+    static get observedAttributes() {
+        return ["enough", "vol"];
+    }
+    attributeChangedCallback(name, oldValue, newValue) {
+        if (name == "enough") {
+            this.setAttribute("select", "false");
+            this.check = false;
+            if (this.getAttribute("enough") == "true") {
+                this.enable = true;
+                this.shadowRoot.getElementById("hour-card").setAttribute("class", "hour-card ");
+            }
+            else {
+                this.enable = false;
+                this.shadowRoot.getElementById("hour-card").setAttribute("class", "hour-card disabled");
+            }
+        }
+        else if (name == "vol") {
+            this.shadowRoot.getElementById("vol").innerText = this.getAttribute("vol");
+        }
+
+
+
+    }
+
+    connectedCallback() {
+        this.shadowRoot
+            .querySelector(".hour-card")
+            .addEventListener("click", () => this.handleClick());
+    }
+    disconnectedCallback() {
+        this.shadowRoot.querySelector(".hour-card").removeEventListener();
+    }
+
+}
+customElements.define("time-card", TimeCard);
+
+
+
+// JS main
+var gagJSON;
+var selectList = [0, 0, 0, 0, 0, 0, 0, 0];
+var user, date, eqt,itemid;
+var inputVol = [40, 10, 10, 20, 10, 30, 10, 20];
+var ToolID;
+var urldate;
+getItemid();
+console.log(itemid);
+if (lablint == false) {
+    //changeCardVol();
+    document
+        .getElementById("sel-book")
+        .addEventListener("click", () => handleBook());
+
+}
+document
+    .getElementById("eqtinput")
+    .addEventListener("change", () => handleEqtchange());
+document
+    .getElementById("sel-search")
+    .addEventListener("click", () => handleSearch());
+document
+    .getElementById("dateinput")
+    .addEventListener("change", () => handleDateChange());
+function changeCardVol() {
+    for (var i = 0; i < 8; i++) {
+        tmpcard = document.getElementById("card" + (i + 1));
+        tmpcard.setAttribute("vol", inputVol[i]);
+    }
+}
+function handleBook() {
+    var i;
+    user = document.getElementById('userinput').value;
+    date = document.getElementById("dateinput").value;
+    eqt = document.getElementById("eqtinput").value;
+    for (i = 0; i < 8; i++) {
+        if (document.getElementById("card" + (i + 1)).getAttribute("select") == "true") {
+            selectList[i] = eqt;
+        }
+        else {
+            selectList[i] = 0;
+        }
+    }
+    
+    if (user == '' || date == '' || eqt == '') {
+        alert("กรุณากรอกข้อมูลให้ครบถ้วน");
+    }
+    else {
+        var quan = "";
+        for (var i = 0; i < 8; i++) {
+            quan = quan + selectList[i];
+        }
+        if (quan == "00000000") {
+            alert("กรุณาเลือกเวลาจอง");
+        }
+        else {
+            var a = document.getElementById("abook");
+            a.setAttribute("href", "/Bookings/Submit?ToolID=" + ToolID + "&date=" + changeDateformat(date) + "&UserID=" + user + "&quan=" + quan);
+            console.log(quan, user, changeDateformat(date), eqt);
+        }
+
+    }
+
+}
+function handleDateChange() {
+    var a = document.getElementById("asearch");
+
+    a.setAttribute("href", "/Bookings?ToolID=" + ToolID + "&date=" + changeDateformat(document.getElementById("dateinput").value))
+}
+
+
+function handleEqtchange() {
+    var i, want, tmpcard;
+
+    want = document.getElementById("eqtinput").value;
+    for (i = 0; i < 8; i++) {
+        tmpcard = document.getElementById("card" + (i + 1));
+        if (want > parseInt(tmpcard.getAttribute("vol"))) {
+            tmpcard.setAttribute("enough", "false");
+        }
+        else {
+            tmpcard.setAttribute("enough", "true");
+        }
+    }
+}
+
+function handleSearch() {
+    date = document.getElementById("dateinput").value;
+    if (date == '') {
+        alert("กรุณากรอก Date");
+    }
+    else {
+        console.log("Searching : " + changeDateformat(date));
+        if (lablint == true) {
+
+            var xhttp = new XMLHttpRequest();
+            xhttp.onreadystatechange = function () {
+                if (this.readyState == 4 && this.status == 200) {
+                    gagJSON = JSON.parse(this.responseText);
+                    //console.log(gagJSON);
+                    handleChangeVol(gagJSON);
+                    handleEqtchange();
+                }
+            }
+            console.log(date);
+            xhttp.open("GET", "https://golablint.azurewebsites.net/api/get-available?id="+itemid+"&date=" + date, true);
+            xhttp.send();
+        }
+    }
+
+}
+function handleChangeVol() {
+    inputVol = gagJSON.time;
+    changeCardVol();
+}
+
+
+function changeDateformat(date) {
+    var tmp = date.split('-');
+
+    return (parseInt(tmp[1]) + '-' + parseInt(tmp[2]) + '-' + parseInt(tmp[0]));
+}
+
+function changrDateformat2(date) {
+    if (date != null) {
+
+        var tmp = date.split('-');
+        var tmpM,tmpD;
+        if (tmp[0] < 10) {
+            tmpM = "0" + tmp[0];
+        }
+        else
+            tmpM = tmp[0];
+
+        if (tmp[1] < 10) {
+            tmpD = "0" + tmp[1];
+        }
+        else
+            tmpD = tmp[1];
+        console.log(tmp[2] + '-' + tmpM + '-' + tmpD);
+        return tmp[2] + '-' + tmpM + '-' + tmpD;
+    }
+}
+
+function getItemid() {
+    const queryString = window.location.search;
+    const urlParams = new URLSearchParams(queryString);
+    itemid = urlParams.get("itemid");
+    ToolID = urlParams.get("ToolID");
+    urldate = urlParams.get("date");
+    setInputDate();
+}
+
+function setInputDate() {
+    document.getElementById("dateinput").value = changrDateformat2(urldate);
+}
